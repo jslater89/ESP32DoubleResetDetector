fb8ba1e409cb6cbf6b67e238fc5aee957e9693cc
Jay Slater <jslater@sdgsystems.com>
2019-09-08 12:55:33 -0400

    Merge

--- examples/DoubleResetDetector_example/DoubleResetDetector_example.ino
+++ examples/DoubleResetDetector_example/DoubleResetDetector_example.ino
@@ -1,9 +1,10 @@
-#include <FS.h>
-#include <DoubleResetDetector.h>
+#include <SPIFFS.h>
+#include "DoubleResetDetector.h"
+
 
 // Number of seconds after reset during which a 
 // subseqent reset will be considered a double reset.
-#define DRD_TIMEOUT 10
+#define DRD_TIMEOUT 3
 
 DoubleResetDetector drd(DRD_TIMEOUT);
 
@@ -17,7 +18,7 @@ void initFS()
 {
   Serial.println("Mounting FS...");
 
-  fsMounted = SPIFFS.begin();
+  fsMounted = SPIFFS.begin(true);
   if (fsMounted) {
     Serial.println("Mounted file system");
     drd.detectDoubleReset(fsMounted);
@@ -31,7 +32,10 @@ void initFS()
 
 void setup()
 {
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.begin(115200);
+  Serial.println();
   Serial.println("DoubleResetDetector Example Program");
   Serial.println("-----------------------------------");
 
@@ -39,8 +43,11 @@ void setup()
 
   if (drd.doubleResetDetected()) {
     Serial.println("Double Reset Detected");
+    digitalWrite(LED_BUILTIN, HIGH);
+    
   } else {
     Serial.println("No Double Reset Detected");
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
 
@@ -52,4 +59,3 @@ void loop()
   // consider the next reset as a double reset.
   drd.loop();
 }
-
